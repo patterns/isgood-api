67d8cb29ddfad46568357da7a9a0f87ae54ad65c
興怡 <>
2021-04-22 00:45:52 -0700

    tf cloud backend

--- tests/terraform/main.tf
+++ tests/terraform/main.tf
@@ -1,9 +1,17 @@
 terraform {
   required_version = ">= 0.12"
 }
+backend "remote" {
+  hostname = "app.terraform.io"
+  organization = "savewhales"
+  workspaces = {
+    name = "isgood-api"
+  }
+}
 
 variable "indicationsfqdn" {
   description = "URL of echo test service"
+  default = "https://www.isgood.ai/"
 }
 
 module "cognito" {
