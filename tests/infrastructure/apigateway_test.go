3a02b2419a14db2a8b13b3b882e49e6f50a19acf
興怡 <>
2021-04-25 21:27:25 -0700

    terratest http req

--- tests/infrastructure/apigateway_test.go
+++ tests/infrastructure/apigateway_test.go
@@ -10,14 +10,20 @@ import (
 func TestApiGateway(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../../tf",
+		Vars: map[string]string {
+			indicationsfqdn: "https://mystifying-swirles-66e451.netlify.app/api/echo",
+		},
 	})
 
 	// Clean up resources with "terraform destroy" at the end of the test.
-	defer terraform.Destroy(t, terraformOptions)
+	////defer terraform.Destroy(t, terraformOptions)
 
 	terraform.InitAndApply(t, terraformOptions)
 
-	output := terraform.Output(t, terraformOptions, "baseUrl")
-	assert.NotZero(t, output)
+	base := terraform.Output(t, terraformOptions, "baseURL")
+
+	// Make an HTTP request to the instance and make sure we get back a 200 OK with the body "Hello, World!"
+	url := fmt.Sprintf("%s/lambda", base)
+	http_helper.HttpGetWithRetry(t, url, nil, 200, "unauthorized", 30, 5*time.Second)
 }
 
