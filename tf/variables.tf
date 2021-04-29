0cca9caaee34feaa3ba8368d5592a772ac1a2f0e
興怡 <>
2021-04-28 23:33:23 -0700

    tf cloud vars

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,10 +1,10 @@
 variable "indicationsfqdn" {
   description = "URL of echo test service"
-  default     = "https://www.isgood.ai/"
+
 }
 
 variable "stagename" {
   description = "API Gateway stage name and appears in baseURL"
-  default     = "dev"
+
 }
 
