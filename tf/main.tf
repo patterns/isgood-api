c269ca378a8057ac10bfb3c4a1b782e462aaa59c
興怡 <>
2021-05-04 20:09:31 -0700

    tf fmt

--- tf/main.tf
+++ tf/main.tf
@@ -8,13 +8,13 @@ module "cognito" {
 }
 
 module "apigateway" {
-  source              = "./modules/apigateway"
-  echo_uri            = var.indicationsfqdn
-  user_pool_arn       = module.cognito.user_pool_arn
+  source        = "./modules/apigateway"
+  echo_uri      = var.indicationsfqdn
+  user_pool_arn = module.cognito.user_pool_arn
   ####example_lambda_arn  = aws_lambda_function.examplefunc.invoke_arn
   example_lambda_name = aws_lambda_function.examplefunc.function_name
 
   # DEBUG DEBUG
-  example_lambda_arn  = var.indicationsfqdn
+  example_lambda_arn = var.indicationsfqdn
 }
 
