598492c3538aa2eb4efb3ab4d9a9f7d950582d93
興怡 <>
2021-05-04 20:50:10 -0700

    no lambda ping

--- tests/infrastructure/ping-test.py
+++ tests/infrastructure/ping-test.py
@@ -10,13 +10,3 @@ def test_endpoint_echo(base):
     body = response.json()
     assert body['projectId'] == testid
 
-def test_endpoint_lambda(base):
-    testid = 'test123'
-    data = {'projectId':testid}
-    response = requests.post(base + '/lambda', json=data)
-
-    assert response.status_code == 401
-    body = response.json()
-    assert body['message'] == 'Unauthorized'
-
-
