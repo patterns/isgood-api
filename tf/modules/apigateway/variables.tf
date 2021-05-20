5ab7ae564478de0e6f04d19480ad89e39a2871f1
興怡 <>
2021-05-19 23:26:30 -0700

    tf fmt

--- tf/modules/apigateway/variables.tf
+++ tf/modules/apigateway/variables.tf
@@ -2,7 +2,7 @@
 variable "echo_uri" {
   description = "FQDN target of HTTP endpoint integration"
   default     = "https://www.isgood.ai/"
-  type = string
+  type        = string
 }
 
 variable "user_pool_arn" { type = string }
