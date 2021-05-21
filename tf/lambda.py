544e047ce566039d982aea624b5460a3efdea620
興怡 <>
2021-05-21 01:06:51 -0700

    overload lambda with 2 routes

--- tf/lambda.py
+++ tf/lambda.py
@@ -1,29 +1,42 @@
 import json
 import random
 
-def handler(event, context):
-        event_body = json.loads(event['body'])
-        param_txt = event_body['projectId']
-        ##offset = event_body['offset']
-        ##limit = event_body['limit']
-        ##name = event_body['name']
-        ##description = event_body['description']
-        ##impacts = event_body['projectImpacts']
-        ##outcomes = event_body['outcomesDesired']
-
-        # size 8 to make diff from the other route
+def recommend_by_project(proj):
+        # fake 8 indicators
         random_pk = random.sample(range(1,100),8)
         accumulator = []
         for pk in random_pk:
             item = {'indicatorId': pk, 'alignedStrength': random.random()}
             accumulator.append(item)
         result = {}
-        result['projectId'] = param_txt
+        result['projectId'] = proj
         result['indicators'] = accumulator
+        return result
+
+
+def dereference_numbers(numbers):
+        # fake indicator details
+        accumulator = []
+        for num in numbers:
+            item = {'id': num, 'description': 'This is a {} indicator description'.format(num)}
+            accumulator.append(item)
+        result = {}
+        result['indicators'] = accumulator
+        return result
+
+
+def handler(event, context):
+        event_body = json.loads(event['body'])
+        event_path = event['path']
+        # overloading handler with multiple routes
+        if '/lambda/mu' in event_path:
+            result = dereference_numbers(event_body['numbers'])
+        else:
+            result = recommend_by_project(event_body['projectId'])
 
         response = {
                'statusCode': 200,
                'body': json.dumps(result)
         }
-
         return response
+
