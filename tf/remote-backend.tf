dec2a87d4ea60d12fece1b82a23c9d8deef5507e
興怡 <>
2021-05-30 08:00:05 -0700

    deploy for /muck route

--- tf/remote-backend.tf
+++ tf/remote-backend.tf
@@ -1,9 +1,9 @@
 
 terraform {
   backend "remote" {
-    organization = "massive-dynamic"
+    organization = "isgood-infra"
     workspaces {
-      name = "dev-sandbox"
+      name = "ghactions-demo"
     }
   }
 }
